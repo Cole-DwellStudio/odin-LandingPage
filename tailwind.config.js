c3ae86712654e6518219476d222c1faf59ce6b55
Cole Harris <cole@dwellstudioco.com>
2022-12-03 17:26:23 -0700

    install and configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'bgdark': '#1F2937',
+      'bgmd': '#E5E7EB',
+      'txtlight': '#F9FAF8',
+      'txtmd': '#E5E7EB',
+      'txtdrk': '#1F2937',
+      'btnblue': '#3883F6',
+    },
     extend: {},
   },
   plugins: [],
